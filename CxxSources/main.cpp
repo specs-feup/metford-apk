a21fe37660a7ec1d44bfc6845ac331b938b6662a
João Bispo <jbispo@fe.up.pt>
2026-02-05 11:56:19 +0000

    Adapts template to run Alpakka instead of Clava

--- CxxSources/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "lib.h"
-
-int main(int argc, char *argv[]) {
-    foo();
-    return 0;
-}
--- src/foo.ts
+++
@@ -1,6 +0,0 @@
-import { FunctionJp } from "@specs-feup/clava/api/Joinpoints.js";
-import Query from "@specs-feup/lara/api/weaver/Query.js";
-
-export function foo() {
-    return Query.search(FunctionJp).get().length;
-};
